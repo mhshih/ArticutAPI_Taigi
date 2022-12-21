bd1ad48e756d1bceeb3cd8af47944c04e8b3e3e3
mhshih <d00142002@ntu.edu.tw>
2022-12-21 14:06:54 +0800

    Add files via upload

---
+++ ArticutAPI0.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+from ArticutAPI import Articut
+from glob import iglob
+import json
+from pprint import pprint
+import re
+import tempfile
+
+from sys import argv
+username = ""
+apikey = ""
+
+articutTaigi = Articut(username, apikey)
+
+import os
+dirname = "dataCollection\\"
+for filename in os.listdir(dirname):
+    if filename.endswith(".html.txt"):
+        f1 = open(dirname+filename, encoding="utf8")
+        f2 = open(dirname+filename+".segmented.txt", "w", encoding="utf8")
+        for line in f1:
+            print(line)
+            try:
+                if line != "\n":
+                    resultDICT = articutTaigi.parse(line, level="lv2")
+                    segmented = resultDICT['result_segmentation']
+                    print(" ".join(segmented.split('/')))
+                    f2.write(" ".join(segmented.split('/')))
+            except:
+                pass
+        f2.close()
+
